f8fb00a791894daf8aa425b8f1926763d092673e
softacoder <113185323+softacoder@users.noreply.github.com>
2025-09-23 12:25:55 +0100

    Commit deploy.ts
    First deploy code in deploy.ts

--- migrations/deploy.ts
+++ migrations/deploy.ts
@@ -1,14 +1,25 @@
-// Migrations are an early feature. Currently, they're nothing more than this
-// single deploy script that's invoked from the CLI, injecting a provider
-// configured from the workspace's Anchor.toml.
+// // Migrations are an early feature. Currently, they're nothing more than this
+// // single deploy script that's invoked from the CLI, injecting a provider
+// // configured from the workspace's Anchor.toml.
+
+// import * as anchor from "@coral-xyz/anchor";
+
+// module.exports = async function (provider: anchor.AnchorProvider) {
+//   // Configure client to use the provider.
+//   anchor.setProvider(provider);
+
+//   // Add your deploy script here.
+// };
 
 import * as anchor from "@coral-xyz/anchor";
+import { Solmultisig } from "../target/types/solmultisig";
 
 module.exports = async function (provider: anchor.AnchorProvider) {
-  // Configure client to use the provider.
   anchor.setProvider(provider);
 
-  // Test changes
-  // Add your deploy script here.
-  // Yes, this is where you'd deploy any programs you have
+  const program = anchor.workspace.solmultisig as Program<Solmultisig>;
+
+  // Deploy program
+  const tx = await program.rpc.initialize(/* params */);
+  console.log("Program deployed:", tx);
 };
